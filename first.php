ac7a04657d6c1316e1e88a52bf1ddc628418652b
Anarchaos <gremendousip@gmail.com>
2021-06-09 20:34:18 +0300

    Верстка

---
+++ first.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Repair</title>
+</head>
+<body>
+	<main class="mainpage">
+		<form action="index.php" class="mainform">
+			<div class = "logocontainer">
+				<img src="icons/logo.png" alt="" class="logo" >
+			</div>
+
+			<div class="authform">
+				<input type="text" name="login" placeholder="Логин" required>
+				<input type="password" name="pass" placeholder="Пароль" required>
+				<button type="submit">→</button>
+			</div>
+		</form>
+	</main>
+</body>
+</html>
