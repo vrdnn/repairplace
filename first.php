2dc90bea8a364ee92f3e23f31936e52a62b3dbde
Anarchaos <gremendousip@gmail.com>
2021-06-19 16:50:30 +0300

    Добавлена авторизация, выход

--- first.php
+++ first.php
@@ -1,5 +1,6 @@
 <?php 
 	require "includes/connection.php";
+	session_start();
 
 	$link = mysqli_connect($host, $user, $password, $database) 
         or die("Ошибка " . mysqli_error($link));
@@ -23,11 +24,25 @@
     	$repair_paystatus[$i] = $row[4];
     	$repair_orderstatus[$i] = $row[5];
     }
+    
+    if (isset($_POST['todaydate']) && isset($_POST['descript']) && isset($_POST['price'])) {
+    	$todaydate=htmlentities(mysqli_real_escape_string($link,$_POST['todaydate']));
+    	$descript=htmlentities(mysqli_real_escape_string($link,$_POST['descript']));
+    	$price=htmlentities(mysqli_real_escape_string($link,$_POST['price']));
 
-    // for ($i=0; $i < $rows ; $i++) { 
-    // 	echo $repair_id[i]."  ".$repair_date[i]."  ".$repair_descript[i]."  ".$repair_price[i]."  ".$repair_paystatus[i]."  ".$repair_orderstatus[i];
-    // 	echo "<br>";
-    // }
+    	if (isset($_POST['checking']) && $_POST['checking'] == 'Yes') {
+    		$checking=htmlentities(mysqli_real_escape_string($link,"Оплачено"));
+    	}
+    	else {
+    		$checking=htmlentities(mysqli_real_escape_string($link,"Не оплачено"));
+    	}
+
+    	$query="INSERT INTO orders VALUES(NULL,'$todaydate','$descript','$price','$checking','В работе')";
+        $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
+
+        mysqli_close($link);
+        header('Location: first.php');
+    }
 ?>
 
 <!DOCTYPE html>
@@ -39,53 +54,92 @@
 	<title>Repair</title>
 </head>
 <body>
-	<header class="page-header">
-		<div>
-			<img src="icons/logo.png" width="168" height="57">
-		</div>
-		<div class="indexmenu">
-			<p>Вартан</p>
-			<img src="icons/avatar.png" width="60" height="60" class="avatar">
-			<img src="icons/arrow.png" width="25" height="25" class="arrownav">
-		</div>
-	</header>
-	<main class="mainindex">
-		<section class="orders">
-			<p>Заказы <font color="#A9A9A9">(3)</font><p>
-		</section>
-		<section class="sorting">
-			<button>Все</button>
-			<button>Активные</button>
-			<button>Завершенные</button>
-			<button>Удаленные</button>
-			<button>Добавить</button>
-		</section>
-		<section class="table">
-			<table>
-				<tr>
-					<th>Номер заказа</th>
-					<th>Дата заказа</th>
-					<th>Описание заказа</th>
-					<th>Стоимость заказа</th>
-					<th>Статус оплаты</th>
-					<th>Статус заказа</th>
-				</tr>
-				<?php 
-					for ($i=0; $i < $rows; $i++) { 
-						?>
+	<div class = first>
+		<header class="page-header">
+			<div>
+				<img src="icons/logo.png" width="168" height="57">
+			</div>
+			<div class="indexmenu">
+				<p class = "login"><?php echo $_SESSION["login"]?></p>
+				<img src="icons/avatar.png" width="60" height="60" class="avatar">
+				<a href = "logout.php"><img src="icons/exit.png" width="25" height="25" class="arrownav"></a>
+			</div>
+		</header>
+		<main class="mainindex">
+			<section class="orders">
+				<p>Заказы <font color="#A9A9A9">(<?php echo $rows ?>)</font></p>
+			</section>
+			<section class="sorting">
+				<button>Все</button>
+				<button>Активные</button>
+				<button>Завершенные</button>
+				<button>Удаленные</button>
+				<button id="myBtn">Добавить</button>
+				<div id="mypopup" class="popup">
+					<div class="popup-content">
+						<div class="popup-header">
+							<h2>Добавление заказа</h2>
+							<span class="close">&times;</span>
+						</div>
+						<form class="popup-form" method = "POST" name="add">
+							<input type="datetime-local" placeholder="Дата" name="todaydate">
+							<textarea placeholder="Описание заказа" name="descript"></textarea>
+							<div class="pricecheck">
+								<input type="text" placeholder="Стоимость" name="price">
+								<input type="checkbox" name="checking" value="Yes">
+								<span> Оплата</span>
+							</div>
+							<button type="submit" name="formSubmit">Добавить</button>
+						</form>
+					</div>
+				</div>
+			</section>
+			<section class="table">
+				<table>
 					<tr>
-						<td align="center"><?php echo $repair_id[$i]?></td>
-						<td align="center"><?php echo $repair_date[$i]?></td>
-						<td align="center"><?php echo $repair_descript[$i]?></td>
-						<td align="center"><?php echo $repair_price[$i]?></td>
-						<?php if ($repair_paystatus[$i] == "Оплачено") { ?>
-							<td align="center" style="color: #32CD32"><strong><?php echo $repair_paystatus[$i]?></strong></td>
-						<?php } else {?> <td align="center" style="color: #DC143C"><strong><?php echo $repair_paystatus[$i]?></strong></td> <?php }?>
-						<td align="center"><?php echo $repair_orderstatus[$i]?></td>
+						<th>Номер заказа</th>
+						<th>Дата заказа</th>
+						<th>Описание заказа</th>
+						<th>Стоимость заказа</th>
+						<th>Статус оплаты</th>
+						<th>Статус заказа</th>
 					</tr>
-				<?php } ?>
-			</table>
-		</section>
-	</main>
+					<?php 
+						for ($i=0; $i < $rows; $i++) { 
+							?>
+						<tr>
+							<td align="center"><?php echo $repair_id[$i]?></td>
+							<td align="center"><?php echo $repair_date[$i]?></td>
+							<td align="center"><?php echo $repair_descript[$i]?></td>
+							<td align="center"><?php echo $repair_price[$i]?></td>
+							<?php if ($repair_paystatus[$i] == "Оплачено") { ?>
+								<td align="center" style="color: #32CD32"><strong><?php echo $repair_paystatus[$i]?></strong></td>
+							<?php } else {?> <td align="center" style="color: #DC143C"><strong><?php echo $repair_paystatus[$i]?></strong></td> <?php }?>
+							<td align="center"><?php echo $repair_orderstatus[$i]?></td>
+						</tr>
+					<?php } ?>
+				</table>
+			</section>
+		</main>
+	</div>
+	<script>
+		let popup = document.getElementById('mypopup'),
+			popupToggle = document.getElementById('myBtn'),
+			popupClose = document.querySelector('.close');
+
+			popupToggle.onclick = function() {
+				popup.style.display="block";
+			};
+
+			popupClose.onclick = function () {
+				popup.style.display="none";
+			}
+
+			window.onclick = function (e) {
+				if(e.target == popup) {
+					popup.style.display="none";
+				}
+			}
+	</script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php 
+	require "includes/connection.php";
+
+	$link = mysqli_connect($host, $user, $password, $database) 
+        	or die("Ошибка " . mysqli_error($link));
+	if(isset($_POST["login"]) && isset($_POST["password"]))
+	{
+		$login = $_POST["login"];
+		$userpassword = $_POST["password"];
+        $query = "SELECT * FROM users WHERE login = '$login' AND password = '$userpassword'";
+        $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
+        $rows = mysqli_num_rows($result);
+        if ($rows == 1) {
+        	session_start();
+        	$_SESSION['login']=$login;
+            $_SESSION['password']=$userpassword;
+            header('Location:first.php');
+                exit();
+        }
+        else 
+		{
+    	echo "ERROR";
+    	}
+	}
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -8,14 +34,14 @@
 </head>
 <body>
 	<main class="mainpage">
-		<form action="index.php" class="mainform">
+		<form action="index.php" class="mainform" method="POST">
 			<div class = "logocontainer">
 				<img src="icons/logo.png" alt="" class="logo" >
 			</div>
 
 			<div class="authform">
 				<input type="text" name="login" placeholder="Логин" required>
-				<input type="password" name="pass" placeholder="Пароль" required>
+				<input type="password" name="password" placeholder="Пароль" required>
 				<button type="submit">→</button>
 			</div>
 		</form>
